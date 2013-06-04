a1b2e7fa4c08e139c79ba0b5497a9dc3110d2190
Zhidong Tian <tdsjtu@gmail.com>
2013-06-04 17:07:12 +0800

    add enFrame.m

---
+++ code/enframe.m
@@ -0,0 +1,16 @@
+function out = enframe(y, frameSize, overlap)
+
+if nargin<1, selfdemo; return; end
+if nargin<3, overlap=0; end
+if nargin<2, frameSize=256; end
+
+y = y(:);
+step = frameSize-overlap;
+frameCount = floor((length(y)-overlap)/step);
+
+out = zeros(frameSize, frameCount);
+for i=1:frameCount,
+	startIndex = (i-1)*step+1;
+	out(:, i) = y(startIndex:(startIndex+frameSize-1));
+end
+
